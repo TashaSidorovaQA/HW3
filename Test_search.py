34037ac64fd31aabec7afa4428f294973a60d64f
Natasha Sidorova <natasha771@mail.ru>
2025-05-29 21:56:51 +0300

    тесты гугл

---
+++ Test_search.py
@@ -0,0 +1,12 @@
+from selene import browser, have
+
+def test_search_positive():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').type('qa.guru').press_enter()
+    browser.element('[id="search-result"]').should(have.text('Курсы тестировщиков'))
+
+
+def test_search_negative():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').type('gjerigeg[og').press_enter()
+    browser.element('[id="search-result"]').should(have.text('По запросу gjerigeg[og ничего не найдено'))

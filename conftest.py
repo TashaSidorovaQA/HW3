305595028fef8308d1525189fe805dc37bff32b2
Natasha Sidorova <natasha771@mail.ru>
2025-05-29 22:32:58 +0300

    фикстура

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 @pytest.fixture(autouse=True)
 def browser_settings():
-    browser.config.window_width = 800
-    browser.config.window_height = 600
+    browser.config.window_width = 1000
+    browser.config.window_height = 900
     yield
     browser.quit()
